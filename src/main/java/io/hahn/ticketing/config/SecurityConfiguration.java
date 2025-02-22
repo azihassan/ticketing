f4a1cd961403bc4297495f770ed1843a1e8c503d
Azi Hassan <azi.hassan@live.fr>
2025-02-22 02:38:37 +0100

    [master] Add openapi spec,swagger UI and initial database schema

---
+++ src/main/java/io/hahn/ticketing/config/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package io.hahn.ticketing.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(requests ->
+                requests.requestMatchers("/**").permitAll()
+        ).build();
+    }
+}
